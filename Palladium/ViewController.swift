1840571fb6e5894154301a152fa8645c06d9b6e4
Eichman <c.eichman@mac-mini-10.dhcp.wustl.edu>
2024-10-29 14:39:14 -0700

    create xcode proj

---
+++ Palladium/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Palladium
+//
+//  Created by Eichman, Charlotte on 10/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
