79d215e7b9629b21e8e4644246c5f6c05ab4ae6d
emily <emilymhao@gmail.com>
2024-11-11 12:23:27 -0800

    optimizations for FPS (teapot is super float now for some reason)

--- Palladium/Renderer.swift
+++ Palladium/Renderer.swift
@@ -12,6 +12,11 @@ import simd
 import UIKit
 
 class Renderer {
+    struct FragmentParams
+    {
+        var color: (Float, Float, Float, Float)  // just like a float4
+    }
+    
     var device: MTLDevice!
     var metalLayer: CAMetalLayer!
     var vertexBuffer: MTLBuffer!
@@ -19,17 +24,36 @@ class Renderer {
     var commandQueue: MTLCommandQueue!
     var mesh: Mesh!
     
+    var fragParams: FragmentParams
+    var projectionParams: ProjectionParams
+    var transformationParams: TransformationParams
+    
     let outputPixelFormat: MTLPixelFormat = .bgra8Unorm
     
     init(view: UIView, mesh: Mesh) {
         self.device = MTLCreateSystemDefaultDevice()
         self.mesh = mesh
+         // the 0001 is a temporary debug thing
+        self.fragParams = FragmentParams(color: (0, 0, 0, 1))
+        self.projectionParams = ProjectionParams(aspectRatio: 1, fovRadians: 0, nearZ: 0.3, farZ: 1000, time: 0)
+        self.transformationParams = TransformationParams(origin: simd_float3(0, -2, 8), rotation: simd_float3(0, 0, 0), scale: simd_float3(1, 1, 1))
         
         setupMetalLayer(for: view)
         setupPipeline()
         setupVertexBuffer()
         self.commandQueue = device.makeCommandQueue()
     }
+    
+    // temp?
+    func updateProjectionParameters() {
+        let aspectRatio = Float(metalLayer.drawableSize.width / metalLayer.drawableSize.height)
+        let fovRadians = Float(45 * (.pi / 180.0))  // 45 degrees field of view, adjust as needed
+        projectionParams.aspectRatio = aspectRatio
+        projectionParams.fovRadians = fovRadians
+        projectionParams.nearZ = 0.3
+        projectionParams.farZ = 1000
+    }
+
     
     private func setupMetalLayer(for view: UIView) {
         metalLayer = CAMetalLayer()
@@ -59,8 +83,24 @@ class Renderer {
     }
     
     func render() {
-        // Set up render target texture
         guard let drawable = metalLayer.nextDrawable() else { return }
+        updateProjectionParameters()
+                
+        // Update colors (consider only if colors need to change)
+        let time = Date().timeIntervalSince1970.magnitude
+        fragParams.color = (
+            Float(sin(1.0 * time) / 2.0 + 0.5),
+            Float(sin(1.1 * time) / 2.0 + 0.5),
+            Float(sin(1.2 * time) / 2.0 + 0.5),
+            1.0
+        )
+        
+        // Update transformation and projection parameters only when needed
+        transformationParams.rotation.y = Float(sin(time) * 2.5)
+        
+        // Create command buffer and encoder as before, and avoid reallocating structures
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
         let renderPassDescriptor = MTLRenderPassDescriptor()
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
@@ -71,42 +111,71 @@ class Renderer {
             alpha: 1.0
             ) // set the vertices untouched by shaders to this default value (i.e. "background")
         
-        // Draw some pretty colors
-        let time = Date().timeIntervalSince1970.magnitude
-        let redValue   = Float(sin(1.0 * time) / 2.0 + 0.5)  // just a sin of the times i guess..
-        let greenValue = Float(sin(1.1 * time) / 2.0 + 0.5)
-        let blueValue  = Float(sin(1.2 * time) / 2.0 + 0.5)
-        // fragment shader params (make sure the corresponding struct definition in the shader is identical)
-        struct FragmentParams
-        {
-            let color: (Float, Float, Float, Float)  // just like a float4
-        }
-        var fragParams = FragmentParams(color: (redValue, greenValue, blueValue, 1.0))
         
-        let aspectRatio = metalLayer.drawableSize.height / metalLayer.drawableSize.width
-        let fovDegrees = 40.0
-        var projectionParams = ProjectionParams(aspectRatio: Float(aspectRatio),
-                                                fovRadians: Float(fovDegrees / 180.0 * Double.pi),
-                                                nearZ: 0.3,
-                                                farZ: 1000.0,
-                                                time: Float(fmod(time, Double.pi * 2.0)))
-        let xPosition = Float(cos(time) * 2.5) + 3.0
-        let yPosition = Float(sin(time) * 2.5)
-        var transformationParams = TransformationParams(origin: simd_float3(0.0, -2.0, 8.0), rotation: simd_float3(0, yPosition, 0), scale: simd_float3(1, 1, 1))
-        let commandBuffer = commandQueue.makeCommandBuffer() // holds one or more render commands
-        // configure render command
+        
         guard let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
-        renderEncoder.setTriangleFillMode(.fill)
-        renderEncoder.setCullMode(.back)
+        
         renderEncoder.setRenderPipelineState(pipelineState)
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         renderEncoder.setVertexBytes(&projectionParams, length: MemoryLayout.size(ofValue: projectionParams), index: 1)
         renderEncoder.setVertexBytes(&transformationParams, length: MemoryLayout.size(ofValue: transformationParams), index: 2)
-        renderEncoder.setFragmentBytes(&fragParams, length: MemoryLayout.size(ofValue: fragParams), index: 0) // set*Bytes is convenient because you can pass a buffer to the shader without having to explicitly create it in Swift with device.makeBuffer(). probably saves system memory too
-        // interpret vertexCount vertices as instanceCount instances of type .triangle
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: mesh.triangles.count * 3, instanceCount: mesh.triangles.count)
+        renderEncoder.setFragmentBytes(&fragParams, length: MemoryLayout.size(ofValue: fragParams), index: 0)
+        
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: mesh.triangles.count * 3)
         renderEncoder.endEncoding()
-        commandBuffer?.present(drawable) // render to scene color (output)
+        
+        commandBuffer?.present(drawable)
         commandBuffer?.commit()
+
+        
+//        // Set up render target texture
+//        guard let drawable = metalLayer.nextDrawable() else { return }
+//        let renderPassDescriptor = MTLRenderPassDescriptor()
+//        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+//        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+//        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(
+//            red: 0.0,
+//            green: 104.0/255.0,
+//            blue: 55.0/255.0,
+//            alpha: 1.0
+//            ) // set the vertices untouched by shaders to this default value (i.e. "background")
+//        
+//        // Draw some pretty colors
+//        let time = Date().timeIntervalSince1970.magnitude
+//        let redValue   = Float(sin(1.0 * time) / 2.0 + 0.5)  // just a sin of the times i guess..
+//        let greenValue = Float(sin(1.1 * time) / 2.0 + 0.5)
+//        let blueValue  = Float(sin(1.2 * time) / 2.0 + 0.5)
+//        // fragment shader params (make sure the corresponding struct definition in the shader is identical)
+//        struct FragmentParams
+//        {
+//            let color: (Float, Float, Float, Float)  // just like a float4
+//        }
+//        var fragParams = FragmentParams(color: (redValue, greenValue, blueValue, 1.0))
+//        
+//        let aspectRatio = metalLayer.drawableSize.height / metalLayer.drawableSize.width
+//        let fovDegrees = 40.0
+//        var projectionParams = ProjectionParams(aspectRatio: Float(aspectRatio),
+//                                                fovRadians: Float(fovDegrees / 180.0 * Double.pi),
+//                                                nearZ: 0.3,
+//                                                farZ: 1000.0,
+//                                                time: Float(fmod(time, Double.pi * 2.0)))
+//        let xPosition = Float(cos(time) * 2.5) + 3.0
+//        let yPosition = Float(sin(time) * 2.5)
+//        var transformationParams = TransformationParams(origin: simd_float3(0.0, -2.0, 8.0), rotation: simd_float3(0, yPosition, 0), scale: simd_float3(1, 1, 1))
+//        let commandBuffer = commandQueue.makeCommandBuffer() // holds one or more render commands
+//        // configure render command
+//        guard let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
+//        renderEncoder.setTriangleFillMode(.fill)
+//        renderEncoder.setCullMode(.back)
+//        renderEncoder.setRenderPipelineState(pipelineState)
+//        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+//        renderEncoder.setVertexBytes(&projectionParams, length: MemoryLayout.size(ofValue: projectionParams), index: 1)
+//        renderEncoder.setVertexBytes(&transformationParams, length: MemoryLayout.size(ofValue: transformationParams), index: 2)
+//        renderEncoder.setFragmentBytes(&fragParams, length: MemoryLayout.size(ofValue: fragParams), index: 0) // set*Bytes is convenient because you can pass a buffer to the shader without having to explicitly create it in Swift with device.makeBuffer(). probably saves system memory too
+//        // interpret vertexCount vertices as instanceCount instances of type .triangle
+//        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: mesh.triangles.count * 3, instanceCount: mesh.triangles.count)
+//        renderEncoder.endEncoding()
+//        commandBuffer?.present(drawable) // render to scene color (output)
+//        commandBuffer?.commit()
     }
 }
