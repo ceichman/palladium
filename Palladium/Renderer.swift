be31416e7ee95e607cc46ac65abf980df9547e7b
emily <emilymhao@gmail.com>
2024-11-11 11:29:16 -0800

    refactor renderer in branch

--- Palladium/Renderer.swift
+++ Palladium/Renderer.swift
@@ -6,3 +6,107 @@
 //
 
 import Foundation
+import Metal
+import QuartzCore
+import simd
+import UIKit
+
+class Renderer {
+    var device: MTLDevice!
+    var metalLayer: CAMetalLayer!
+    var vertexBuffer: MTLBuffer!
+    var pipelineState: MTLRenderPipelineState!
+    var commandQueue: MTLCommandQueue!
+    var mesh: Mesh!
+    
+    let outputPixelFormat: MTLPixelFormat = .bgra8Unorm
+    
+    init(view: UIView, mesh: Mesh) {
+        self.device = MTLCreateSystemDefaultDevice()
+        self.mesh = mesh
+        
+        setupMetalLayer(for: view)
+        setupPipeline()
+        setupVertexBuffer()
+        self.commandQueue = device.makeCommandQueue()
+    }
+    
+    private func setupMetalLayer(for view: UIView) {
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = outputPixelFormat
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+    }
+    
+    private func setupPipeline() {
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let vertexShader = defaultLibrary.makeFunction(name: "project_vertex")
+        let fragmentShader = defaultLibrary.makeFunction(name: "basic_fragment")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexShader
+        pipelineStateDescriptor.fragmentFunction = fragmentShader
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = outputPixelFormat
+        
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    }
+    
+    private func setupVertexBuffer() {
+        let (vertexArray, dataSize) = self.mesh.vertexArray()
+        vertexBuffer = device.makeBuffer(bytes: vertexArray, length: dataSize, options: [])
+    }
+    
+    func render() {
+        // Set up render target texture
+        guard let drawable = metalLayer.nextDrawable() else { return }
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(
+            red: 0.0,
+            green: 104.0/255.0,
+            blue: 55.0/255.0,
+            alpha: 1.0
+            ) // set the vertices untouched by shaders to this default value (i.e. "background")
+        
+        // Draw some pretty colors
+        let time = Date().timeIntervalSince1970.magnitude
+        let redValue   = Float(sin(1.0 * time) / 2.0 + 0.5)  // just a sin of the times i guess..
+        let greenValue = Float(sin(1.1 * time) / 2.0 + 0.5)
+        let blueValue  = Float(sin(1.2 * time) / 2.0 + 0.5)
+        // fragment shader params (make sure the corresponding struct definition in the shader is identical)
+        struct FragmentParams
+        {
+            let color: (Float, Float, Float, Float)  // just like a float4
+        }
+        var fragParams = FragmentParams(color: (redValue, greenValue, blueValue, 1.0))
+        
+        let aspectRatio = metalLayer.drawableSize.height / metalLayer.drawableSize.width
+        let fovDegrees = 40.0
+        var projectionParams = ProjectionParams(aspectRatio: Float(aspectRatio),
+                                                fovRadians: Float(fovDegrees / 180.0 * Double.pi),
+                                                nearZ: 0.3,
+                                                farZ: 1000.0,
+                                                time: Float(fmod(time, Double.pi * 2.0)))
+        let xPosition = Float(cos(time) * 2.5) + 3.0
+        let yPosition = Float(sin(time) * 2.5)
+        var transformationParams = TransformationParams(origin: simd_float3(0.0, -2.0, 8.0), rotation: simd_float3(0, yPosition, 0), scale: simd_float3(1, 1, 1))
+        let commandBuffer = commandQueue.makeCommandBuffer() // holds one or more render commands
+        // configure render command
+        guard let renderEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
+        renderEncoder.setTriangleFillMode(.fill)
+        renderEncoder.setCullMode(.back)
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder.setVertexBytes(&projectionParams, length: MemoryLayout.size(ofValue: projectionParams), index: 1)
+        renderEncoder.setVertexBytes(&transformationParams, length: MemoryLayout.size(ofValue: transformationParams), index: 2)
+        renderEncoder.setFragmentBytes(&fragParams, length: MemoryLayout.size(ofValue: fragParams), index: 0) // set*Bytes is convenient because you can pass a buffer to the shader without having to explicitly create it in Swift with device.makeBuffer(). probably saves system memory too
+        // interpret vertexCount vertices as instanceCount instances of type .triangle
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: mesh.triangles.count * 3, instanceCount: mesh.triangles.count)
+        renderEncoder.endEncoding()
+        commandBuffer?.present(drawable) // render to scene color (output)
+        commandBuffer?.commit()
+    }
+}
